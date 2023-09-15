670d0ece2170455ae7038c53063c699336db8c58
Ericacdev <nechipato@gmail.com>
2023-09-15 14:15:39 -0700

    5 Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  main - a program that prints with proper  
+ *   grammar using the function printf 
+ *    but the outcome is a piece of art,
+ *     Return: 0 
+ */      
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
