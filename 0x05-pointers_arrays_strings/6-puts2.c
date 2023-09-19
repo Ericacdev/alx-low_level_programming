468cf57bf19cd3edb17c851c65debde857fad126
Ericacdev <nechipato@gmail.com>
2023-09-19 14:15:30 -0700

    6 puts update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *puts2 - prints every other character of a string, starting with the first character
+ *puts2 - prints every other character of a string
  *followed by a new line
  *@str: string to print the char
  */
