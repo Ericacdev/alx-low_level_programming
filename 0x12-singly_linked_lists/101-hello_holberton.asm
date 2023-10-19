e0c29ff2a10f623c719322efb123df8275d7fb1e
Ericacdev <nechipato@gmail.com>
2023-10-19 16:23:54 -0700

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,15 @@
-section .data
-    hello_string db "Hello, Holberton",0
-
+section.data
+hello_string db "Hello, Holberton",0
 section .text
-    global main
+global main
 
 extern printf
 main:
-    push rdi         ; Save registers
-    push rsi
-    mov rdi, hello_string  ; Load address of hello_string into rdi
-    call printf      ; Call printf
-    pop rsi          ; Restore registers
+push rdi;Save registers
+push rsi
+mov rdi,hello_string;Load address of hello_string into rdi
+    call printf;Call printf
+    pop rsi;Restore registers
     pop rdi
-    xor eax, eax     ; Return 0
+    xor eax, eax; Return 0
     ret
