f687c06301422eeb09ecc41f5a968dfa21e58050
Ericacdev <nechipato@gmail.com>
2023-10-23 12:23:08 -0700

    print file added

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints a linked list
+ * @h: pointer of head lost
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t elem = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		elem++;
+		h = h->next;
+	}
+
+	return (elem);
+}
